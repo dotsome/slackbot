8ab9e899aeb9e7bd9fa5b7456bca931badcffc98
Daisuke Saito <saito@lee-lab.org>
2026-03-17 15:25:00 +0900

    FastAPIとSlackBoltを使用して、Slackイベントを処理する基本的な構成を追加

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.fastapi import SlackRequestHandler
+from fastapi import FastAPI, Request
+
+s_app = App(token=os.getenv('SLACK_BOT_TOKEN'), signing_secret=os.getenv('SLACK_SIGNING_SECRET'))
+
+app = FastAPI()
+app_handler = SlackRequestHandler(s_app)
+
+
+@app.post("/slack/events")
+async def events(request: Request):
+    return await app_handler.handle(request)
+
 # SlackBoltインスタンスで、コマンド「/demo」の実行を検知
 @s_app.command('/semihelper')
 def demo(ack, respond, command):
